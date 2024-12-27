4be32cc35017e2740a2c94429b3f336dd0f5f91d
DimensionStudios <piggyandbobo@icloud.com>
2024-12-27 13:51:06 -0800

    added very basic money addition

--- frontend.py
+++ frontend.py
@@ -2,13 +2,28 @@ from tkinter import *
 
 #a4a4a4 for dark, #c1c1c1 for light
 
+moneyamount = 0
+moneylabel = None
+
+def addone():
+    global moneyamount, moneylabel
+    moneyamount += 1
+    if moneylabel:
+        moneylabel.destroy()
+    moneylabel = Label(root, text=moneyamount)
+    moneylabel.pack()
+
+
 def main():
     #Setup for window
+    global root
     root = Tk()
     root.title("Bank")
     root.geometry('860x640')
     root.config(bg='#c1c1c1')
 
     #Interactive parts
+    addmoneybutton = Button(root, text="Add $1", command=addone)
+    addmoneybutton.pack()
 
     root.mainloop()
