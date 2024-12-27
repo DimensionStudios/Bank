552570a7d1493ca5e9502120cc6ff6a4c54a61bc
DimensionStudios <piggyandbobo@icloud.com>
2024-12-27 13:38:01 -0800

    added notes, in lightmode

--- frontend.py
+++ frontend.py
@@ -1,8 +1,14 @@
 from tkinter import *
 
+#a4a4a4 for dark, #c1c1c1 for light
+
 def main():
+    #Setup for window
     root = Tk()
     root.title("Bank")
     root.geometry('860x640')
-    root.config(bg='#4a4a4a')
+    root.config(bg='#c1c1c1')
+
+    #Interactive parts
+
     root.mainloop()
